8c67174556052862dcb8733349ddb83f35f088bc
shanmukh-sai2003 <koradashanmukhasai10@gmail.com>
2024-02-03 09:31:23 +0530

    Add control buttons

--- index.js
+++ index.js
@@ -2,25 +2,58 @@ console.log("etch-a-sketch");
 
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const penColorPicker = document.querySelector(".pen-color");
+const gridSizeSelector = document.querySelector(".grid-size");
+const eraserBtn = document.querySelector(".eraser");
+const penBtn = document.querySelector(".pen");
+const clearBtn = document.querySelector(".clear");
 
+let penColor = "black";
 let mouseDown = false;
+let gridSize = 16;
+let isEraser = false;
+
+constructGrid(gridSize);
+
+penColorPicker.addEventListener("change", (e) => {
+    penColor = e.target.value;
+});
+
+gridSizeSelector.addEventListener("change", (e) => {
+    gridSize = e.target.value;
+    container.innerHTML = "";
+    constructGrid(gridSize);
+});
+
 body.addEventListener("mousedown", () => mouseDown = true);
 body.addEventListener("mouseup", () => mouseDown = false);
 
-let gridSize = Number(prompt("enter grid size: "));
+eraserBtn.addEventListener("click", () => {
+    isEraser = !isEraser;
+});
 
-for(let i = 0; i < gridSize; i++) {
-    for(let j = 0; j < gridSize; j++) {
-        const pixel = document.createElement("div");
-        pixel.className = "pixel";
-        pixel.style.width = `${500 / gridSize}px`;
-        pixel.style.height = `${500 / gridSize}px`;
-        container.appendChild(pixel);
-        pixel.addEventListener('mouseover', () => {
-            if(mouseDown) {
-                pixel.style.backgroundColor = "black";
-            }
-        });
+penBtn.addEventListener("click", () => isEraser = false);
+
+clearBtn.addEventListener("click", () => {
+    container.innerHTML = "";
+    constructGrid(gridSize);
+});
+
+function constructGrid(gridSize) {
+    for(let i = 0; i < gridSize; i++) {
+        for(let j = 0; j < gridSize; j++) {
+            const pixel = document.createElement("div");
+            pixel.className = "pixel";
+            pixel.style.width = `${500 / gridSize}px`;
+            pixel.style.height = `${500 / gridSize}px`;
+            container.appendChild(pixel);
+            pixel.addEventListener('mouseover', () => {
+                if(mouseDown && isEraser) {
+                    pixel.style.backgroundColor = "#fff";
+                } else if(mouseDown) {
+                    pixel.style.backgroundColor = penColor;
+                }
+            });
+        }
     }
 }
-
